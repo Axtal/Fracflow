9b5fb97dab5357c4c99024a9d3ec3f9790993278
Sergio <s.torres@westlake.edu.cn>
2021-09-08 16:24:26 +0800

    coral.m updated

--- coral.m
+++ coral.m
@@ -1,12 +1,14 @@
 sol = h5read('Coral-Trial.h5','/t0/channel0');
 dims = size(sol);
 
+
+
 nx0 = 200;
-ny0 = 300;
-nz0 = 50;
+ny0 = 450;
+nz0 = 0;
 nx  = 300;
-ny  = 300;
-nz  = 1;
+ny  = 1;
+nz  = 100;
 step = 1;
 
 ixpt = dims(1)-(nx0:nx0+nx-1);
@@ -16,24 +18,18 @@ ix = (nx0:nx0+nx-1);
 iy = (ny0:ny0+ny-1);
 iz = (nz0:nz0+nz-1);
 
-solpt = sol(ixpt,iypt,izpt);
+solpt = rot90(flip(sol(ixpt,iypt,izpt),1),-1);
 
 [x,y,z] = meshgrid(ix,iy,iz);
 
 idx=find(solpt==1);
 
-% xp = -y(idx)+min(y(idx))+max(y(idx));
-% yp = -x(idx)+min(x(idx))+max(x(idx));
-xt = x(idx(1:step:end));
-yt = y(idx(1:step:end));
+xp = x(idx(1:step:end));
+yp = y(idx(1:step:end));
 zp = z(idx(1:step:end));
 
-xp = nx0 +(yt-ny0);
-yp = ny0 +(xt-nx0);
-
 figure(1)
-plot3(xp,yp,zp,'o'); %use this for z planes
-%plot3(xt,yt,zp,'o'); %use this for y planes
+plot3(xp,yp,zp,'.'); %use this for z planes
 hold on;
 axis square;
 xlabel('x');
